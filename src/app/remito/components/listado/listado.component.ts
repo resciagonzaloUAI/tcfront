94252792cc85c7441f62ea73dbf0ed4c7eea7470
resciagonzaloUAI <gonzalonicolas.rescia@alumnos.uai.edu.ar>
2023-08-06 20:11:05 -0300

    "Cambiando info de listados + acomodando Menu"

--- src/app/remito/components/listado/listado.component.ts
+++ src/app/remito/components/listado/listado.component.ts
@@ -2,6 +2,9 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { noop, tap } from 'rxjs';
 import { Router } from '@angular/router';
 import { RemitoService } from '../../services/remito.service';
+import { FacturaService } from 'src/app/factura/services/factura.service';
+import { Factura } from 'src/app/shared/types/Factura';
+import { ClienteService } from 'src/app/cliente/services/cliente.service';
 
 @Component({
   selector: 'app-listado',
@@ -10,12 +13,17 @@ import { RemitoService } from '../../services/remito.service';
 })
 export class ListadoComponent implements OnInit {
   remitos: any[] = [];
+  facturas: any[] = [];
+  idClienteRem: number = 0;
+  clientes: any[] = [];
   headers: Array<string> = ['Nro. Remito', 'Nro. Factura', 'Importa', 'Estado'];
 
   constructor(
     private readonly remitoService: RemitoService,
+    private readonly facturaService: FacturaService,
     private readonly cd: ChangeDetectorRef,
-    private readonly router: Router
+    private readonly router: Router,
+    private readonly clienteService: ClienteService
   ) {}
 
   ngOnInit(): void {
@@ -28,9 +36,33 @@ export class ListadoComponent implements OnInit {
       .pipe(
         tap((data) => {
           this.remitos = data.map((rem) => {
+            /*    this.facturaService
+              .getById(rem.idcfactura)
+              .pipe(
+                tap((fact) => {
+                  this.facturas = fact.map((fc) => {
+                    const idClienteRem = fc.idCliente;
+                  });
+                })
+              )
+              .subscribe(noop);
+          
+            this.clienteService
+              .getById(this.idClienteRem)
+              .pipe(
+                tap((cliente) => {
+                  this.clientes = cliente.map((cl) => {
+                    const nombreCliente = cl.nombreCliente;
+                    return nombreCliente;c
+                  });
+                })
+              )
+              .subscribe(noop);
+*/
             return {
               'Nro. Remito': rem.idremito,
               'Nro. Factura': rem.idcfactura,
+              // 'Nombre Cliente': nombreCliente,
               Importa: rem.importeFinal,
               Estado: rem.estado,
             };
